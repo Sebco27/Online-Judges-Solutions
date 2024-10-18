920245b120fed6631aff5bdf584485c168446a4b
Sebas Quesada <167464285+Sebco27@users.noreply.github.com>
2024-10-17 20:15:05 -0600

    Update Kattis Neighborhood Watch Solution
    EN: Replacement of unnecessary "long long".
    
    ES: Reemplazo de "long long" innecesarios.

--- Kattis/Neighborhood Watch.cpp
+++ Kattis/Neighborhood Watch.cpp
@@ -2,12 +2,13 @@
 using namespace std;
 
 int main() {
-    long long int N, K, H1=0, H2, res; // H1 & H2 = temp house positions
+    long long int N, K, H1=0, H2; // H1 & H2 = temp houses (as we go through the neighborhood)
+    int res;
     cin >> N >> K;
-    res = (N*(N+1))/2;
+    res = (N*(N+1))/2; // This will be the total if all houses are watch houses
     if(K) cin >> H1;
     res -= ((H1-1)*(H1))/2;
-    for(long long int i = 1; i<K; i++){
+    for(int i = 1; i<K; i++){
         cin >> H2;
         res -= ((H2-H1-1)*(H2-H1))/2;
         H1 = H2;
